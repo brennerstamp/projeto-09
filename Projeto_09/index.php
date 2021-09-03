7e76e99d79b3f0888ad3e052d83633c75443cc78
brennerstamp <87397409+brennerstamp@users.noreply.github.com>
2021-09-03 12:54:05 -0300

    Add files via upload

---
+++ Projeto_09/index.php
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 09</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale1.0">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+</head>
+<body>
+	<ul class="nav-desktop">
+			<li><span></span><a href="">Invisalign</a></li>
+			<li><span></span><a href="">Benefícios</a></li>
+			<li><span></span><a href="">Serve para mim?</a></li>
+			<li><span></span><a href="">Contato</a></li>
+	</ul>
+	<section class="header-master">
+		<header>
+			<div class="container">
+				<div class="logo"></div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li class="selected"><span></span><a href="">Invisalign</a></li>
+						<li><span></span><a href="">Benefícios</a></li>
+						<li><span></span><a href="">Serve para mim?</a></li>
+						<li><span></span><a href="">Contato</a></li>
+					</ul>
+				</nav>
+				<nav class="mobile">
+					<h3><i class="fa fa-bars"></i></h3>
+				</nav>
+				<div class="telefone-header">
+					<span>(48) 3222-6591</span>
+				</div><!--telefone-->
+				<div class="clear"></div><!--clear-->
+			</div><!--container-->
+		</header>
+
+<!-------------------------------------------------------------------------->
+
+	<div class="conteudo-header">
+		<div class="container">
+			<div class="um">
+				<h2>Seu sorriso perfeito através de um tratamento discreto e inovador, em Florianopolis</h2>
+				<p>Alinhe seus dentes com conforto, praticidade e liberdade! <b>Deixe seu email</b> para que a Dra. Leda possa entrar em contato e <b>agendar uma avaliação gratuita.</b></p>
+				<form>
+					<input type="email" name="email">
+					<input type="submit" name="acao" value="Quero saber mais!">
+					<span>Ou ligue para a clinica Monguilhott (48) 32222-6591</span>
+			
+				<div class="clear"></div><!--clear-->
+			</div><!--dois-->
+				</form>
+			</div><!--um-->
+
+			<div class="dois">
+				<img src="imagens/round_images/circle03.png">
+				<img src="imagens/round_images/circle04.png">
+				<img src="imagens/round_images/circle01.png">
+				<img src="imagens/round_images/circle02.png">
+			<div class="clear"></div><!--clear-->
+		</div><!--container-->
+	</div><!--conteudo-header-->
+
+</section><!--header-master-->
+<!------------------------------------------------------------------------------>
+
+	<section class="diferenciais">
+		<div class="container">
+			<div class="diferencial-single1">
+				<img src="imagens/icones/icone04.png" />
+				<h2>Discreto por ser <b>transparente</b></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum.</p>
+			</div><!--diferencial-single-->
+
+			<div class="diferencial-single2">
+				<img src="imagens/icones/icone02.png" />
+				<h2>Móvel, <b>não atrapalha</b> para comer ou beber</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum.</p>
+			</div><!--diferencial-single-->
+
+			<div class="diferencial-single3">
+				<img src="imagens/icones/icone03.png" />
+				<h2>Livre de fios ou metais, <b>muito mais conforto!</b></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum.</p>
+				<style type="text/css">.diferencial-single h2 {top:  -10px;} </style>
+			</div><!--diferencial-single-->
+
+			<div class="diferencial-single4">
+				<img src="imagens/icones/icone06.png" />
+				<h2>Consultas e <b>resultados mais rápidos</b></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum.</p>
+			</div><!--diferencial-single-->
+
+			<div class="diferencial-single5">
+				<img src="imagens/icones/icone01.png" />
+				<h2>Não atrapalha para <b>falar ou cantar :)</b></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum.</p>
+			</div><!--diferencial-single-->
+
+			<div class="diferencial-single6">
+				<img src="imagens/icones/icone05.png" />
+				<h2>Veja o resultado <b>antes de começar!</b></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum.</p>
+			</div><!--diferencial-single-->
+
+		</div><!--container-->
+	</section><!--diferenciais-->
+
+<!------------------------------------------------------------------------------>
+<section class="sobre">
+	
+	<div class="container">
+
+		<div class="sobre-container">
+			<img src="imagens/rosto.png">
+		</div><!--sobre1-->
+
+		<div class="sobre-container">
+			
+			<h2>O futuro chegou, e ele é transparente!</h2>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum. Praesent sodales lacinia ipsum.</p>
+
+			<h2>Conheça o procedimento com a<br> <b>Dra. Leda Monguilhott</b></h2>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed consequat tortor. Praesent sodales lacinia ipsum. Praesent sodales lacinia ipsum.</p>
+			<div class="clear"></div><!--clear-->
+	</div><!--container-->
+
+</section><!--sobre-container-->
+<!------------------------------------------------------------------------------>
+
+<section class="depoimento">
+
+	<div class="container">
+		
+     <h2>"Sensação de liberdade é perceber que você não está amarrado em nada"</h2>
+     <p>Invisalign</p>
+
+
+	</div><!--container-->
+
+</section><!--depoimento-->
+
+<!------------------------------------------------------------------------------>
+
+<section class="etapas">
+	
+	<div class="container">
+
+		<h2>Sem duvidas, <b>serve para você</b>!<br> Veja como funciona:</h2>
+		
+		<div class="etapas__single">
+			
+			<div class="etapas__group">
+				<img src="imagens/etapas/round_etapas_01.jpg">
+				<p>Aparelho tradicional e Ivisalign</p>
+			<div class="clear"></div><!--clear-->
+			</div><!--etapas__group-->
+
+			<div class="etapas__group">
+				<img style= "float: right;  width: 35%;"  src="imagens/etapas/round_etapas_02.jpg">
+				<div class="clear"></div><!--clear-->
+			</div><!--etapas__group-->
+
+			<div class="etapas__group">
+				<img style="float:left;" src="imagens/etapas/round_etapas_03.jpg">
+				<div class="clear"></div><!--clear-->
+			</div><!--etapas__group-->
+
+		</div><!--etapas__single-->
+
+
+		<div  style="margin-top:50px;padding:0 30px;" class="etapas__single">
+			
+			<div class="etapas__group2">
+				
+				<div class="circle">
+					<div class="line"></div><!--line-->	
+					<span>1</span>
+				</div><!--circle-->
+				<div class="etapas__descricao">
+					<h3>Loren ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida, purus at cursus consectetur, sapien nisi commodo elit, a tempus augue metus eu lorem.</p>
+				</div><!--etapas__descricao-->
+<div class="clear"></div><!--clear-->
+			</div><!--etapas__group2-->
+
+<!------divisão circle----------->
+
+				<div class="etapas__group2">
+				
+				<div class="circle">
+				<div class="line"></div><!--line-->	
+					<span>2</span>
+				</div><!--circle-->
+				<div class="etapas__descricao">
+					<h3>Loren ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida, purus at cursus consectetur, sapien nisi commodo elit, a tempus augue metus eu lorem.</p>
+				</div><!--etapas__descricao-->
+<div class="clear"></div><!--clear-->
+			</div><!--etapas__group2-->
+
+<!------divisão circle----------->
+
+
+				<div class="etapas__group2">
+				
+				<div class="circle">
+				<div class="line"></div><!--line-->	
+					<span>3</span>
+				</div><!--circle-->
+				<div class="etapas__descricao">
+					<h3>Loren ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida, purus at cursus consectetur, sapien nisi commodo elit, a tempus augue metus eu lorem.</p>
+				</div><!--etapas__descricao-->
+<div class="clear"></div><!--clear-->
+			</div><!--etapas__group2-->
+
+<!------divisão circle----------->
+
+
+				<div class="etapas__group2">
+				
+				<div class="circle">
+				<div class="line"></div><!--line-->	
+					<span>4</span>
+				</div><!--circle-->
+				<div class="etapas__descricao">
+					<h3>Loren ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida, purus at cursus consectetur, sapien nisi commodo elit, a tempus augue metus eu lorem.</p>
+				</div><!--etapas__descricao-->
+<div class="clear"></div><!--clear-->
+			</div><!--etapas__group2-->
+
+<!------divisão circle----------->
+
+
+				<div class="etapas__group2">
+				
+				<div class="circle">
+				<div class="line"></div><!--line-->
+					<span>5</span>
+				</div><!--circle-->
+				<div class="etapas__descricao">
+					<h3>Loren ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida, purus at cursus consectetur, sapien nisi commodo elit, a tempus augue metus eu lorem.</p>
+				</div><!--etapas__descricao-->
+<div class="clear"></div><!--clear-->
+			</div><!--etapas__group2-->
+
+<!------divisão circle----------->
+
+				<div class="etapas__group2">
+				
+				<div class="circle">
+					<span>6</span>
+				</div><!--circle-->
+				<div class="etapas__descricao">
+					<h3>Loren ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida, purus at cursus consectetur, sapien nisi commodo elit, a tempus augue metus eu lorem.</p>
+				</div><!--etapas__descricao-->
+<div class="clear"></div><!--clear-->
+			</div><!--etapas__group2-->
+
+		</div><!--etapas__single-->
+
+		<div class="clear"></div><!--clear-->
+	</div><!--container-->
+
+</section><!--etapas-->
+
+<!---------------------------------------------------------------------------------------->
+
+<section class="diferencas">
+
+	<div class="container">
+
+		<div class="diferencas__single" style="border-right: 1px solid #646464;">
+			<h2>Tradicional</h2>
+			 <div class="item-diferencas-1">
+			 	
+ 				<div class="line-diferencas-1"></div><!--line-diferencas-1-->
+ 				<span>Aparelho Fixo</span>
+
+			 </div><!--item-diferencas-1-->
+<div></div>
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-1">
+			 	
+ 				<div class="line-diferencas-1"></div><!--line-diferencas-1-->
+ 				<span>Acumula sujeiras</span>
+
+			 </div><!--item-diferencas-1-->
+<div></div>
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-1">
+			 	
+ 				<div class="line-diferencas-1"></div><!--line-diferencas-1-->
+ 				<span>Incomoda esteticamente</span>
+
+			 </div><!--item-diferencas-1-->
+<div></div>
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-1">
+			 	
+ 				<div class="line-diferencas-1"></div><!--line-diferencas-1-->
+ 				<span>Demora a ver resultados</span>
+
+			 </div><!--item-diferencas-1-->
+<div></div>
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-1">
+			 	
+ 				<div class="line-diferencas-1"></div><!--line-diferencas-1-->
+ 				<span>Atrapalha para comer</span>
+
+			 </div><!--item-diferencas-1-->
+<div></div>
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-1">
+			 	
+ 				<div class="line-diferencas-1"></div><!--line-diferencas-1-->
+ 				<span>desconfortáveis</span>
+
+			 </div><!--item-diferencas-1-->
+<div></div>
+		</div><!--diferencas__single-->
+
+
+
+
+
+
+<!-------------------diferencas_single2----------->
+
+<div class="diferencas__single">
+			<img src="imagens/invisalign.jpg">
+			 <div class="item-diferencas-2">
+			 	
+ 				<span>Completamente <b>movel</b></span>
+
+			 </div><!--item-diferencas-2-->
+
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-2">
+
+ 				<span>Tira para comer e pode lavar</span>
+
+			 </div><!--item-diferencas-2-->
+			 
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-2">
+			 	
+ 			
+ 				<span><b>100% transparente</b></span>
+
+			 </div><!--item-diferencas-2-->
+
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-2">
+			 	
+ 			
+ 				<span>Resultados rapidos</span>
+
+			 </div><!--item-diferencas-2-->
+
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-2">
+			 	
+ 			
+ 				<span>Muito confortavel</span>
+
+			 </div><!--item-diferencas-2-->
+
+<!---------------------divisão diferenças------------------------------------------------->
+			  <div class="item-diferencas-2">
+			 	
+ 			
+ 				<span>Gengivas mais saudaveis</span>
+
+			 </div><!--item-diferencas-2-->
+
+<!---------------------divisão diferenças------------------------------------------------->
+
+		</div><!--diferencas__single-->		
+		<div class="clear"></div>
+	</div><!--container-->
+</section><!--diferencas-->
+
+<!--------------------------------------------------------------------------------------->
+
+<section class="contato">
+	
+	<div class="container">
+
+		<h2>Entre em contato</h2>
+
+		<p style=" margin-top: 180px;">Telefone: (48) 3222-6591
+			<br />
+			Email: contato@monguilhott.com.br
+			<br />
+			Endereço:Avenida Rio Branco, 354 Edifício Maxim's 3° Andar
+			/Sala 301 - Florianópolis/SC</p>
+
+		
+		<form>
+			
+			<div class="form-group">
+				<input placeholder="Nome"  type="nome" name="Nome" required>
+			</div><!--form-group-->
+
+			<div class="form-group">
+				<input placeholder="Email"  type="email" name="email" required>
+			</div><!--form-group-->
+
+			<div class="form-group">
+				<input placeholder="Telefone"  type="telefone" name="telefone" required>
+			</div><!--form-group-->
+
+			<div class="form-group">
+			<textarea placeholder="Mensagem..." name="mensagem"></textarea>
+			</div><!--form-group-->
+			<div class="form-group">
+				<input class="botao" type="submit" name="acao" value="Enviar">
+			</div><!--form-group-->
+		</form>
+
+	</div><!--container-->
+<div class="clear"></div>
+</section><!--contato-->
+
+
+<footer>
+	<div class="container">
+		<img src="imagens/logo.png">
+	</div><!--container-->	
+
+</footer>
+
+
+
+<!--jquery-->
+    <script type="text/javascript">
+    	$('nav.desktop a').hover(function(){
+    		$('nav li').removeClass('selected');
+    		$(this).parent().addClass('selected');})
+
+			$('nav.mobile h3').click(function(){
+    		$('ul.nav-desktop').slideToggle();
+    		$('nav.mobile a').hover(function(){
+    		$('nav li').removeClass('selected');
+    		$(this).parent().addClass('selected');})
+    	})
+
+    </script>
+
+</body>
+</html>
